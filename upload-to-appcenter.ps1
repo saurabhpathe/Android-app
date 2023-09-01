3526fcc7dfdb14d94cb313fc9b5c23e6f936ccb6
saurabhpathe <95735601+saurabhpathe@users.noreply.github.com>
2023-09-01 21:31:46 +0530

    Create upload-to-appcenter.ps1

---
+++ upload-to-appcenter.ps1
@@ -0,0 +1,17 @@
+$token = "739256c4c5772f5c2840fa51d4f4912bf62be1c0"
+$file_path = "$(System.ArtifactsDirectory)/app-debug.apk"
+$app_name = "Android-App"
+$group = "Androidapp"
+$release_notes = "Automated build from Azure DevOps"
+
+$body = @{
+  token = $token
+  file_path = $file_path
+  app_name = $app_name
+  group = $group
+  release_notes = $release_notes
+} | ConvertTo-Json
+
+$uri = "https://api.appcenter.ms/v0.1/apps/saurabhpathe/Android-App/uploads/releases"
+
+Invoke-RestMethod -Uri $uri -Method Post -Headers @{"Content-Type"="application/json"} -Body $body
